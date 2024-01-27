1a78c6b69cf8605800932f494a81d4e9016fb33e
Kamil Mirosz <kamil.mirosz@nopio.com>
2024-01-27 22:58:56 +0100

    Add ColorsPalette utility enum

---
+++ src/utils/ColorsPalette.tsx
@@ -0,0 +1,15 @@
+enum ColorsPalette {
+    red = '#FF0000', // Classic Uno red
+    yellow = '#FFFF00', // Bright yellow
+    green = '#008000', // Uno green
+    blue = '#0000FF', // Uno blue
+    lightRed = '#FF6666', // Lighter shade of red
+    lightYellow = '#FFFF99', // Lighter shade of yellow
+    lightGreen = '#66FF66', // Lighter shade of green
+    lightBlue = '#6699FF', // Lighter shade of blue
+    actionBlack = '#000000', // Black for action cards
+    white = '#FFFFFF', // White for text or designs
+    transparent = 'transparent', // Transparent for special cases
+}
+
+export default ColorsPalette;
