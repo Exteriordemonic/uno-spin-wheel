38cd6f54c3b0f2856cf3a801094b0d27282302ec
Kamil Mirosz <kamil.mirosz@nopio.com>
2024-01-30 19:58:22 +0100

    Add PointerElement component to Wheal container

--- src/containers/wheal/index.tsx
+++ src/containers/wheal/index.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import WedgeLabel from "../../components/wedge-label";
 import CaclWedeSize from "../../utils/CalcWedgeSize";
 import Logo from "../../components/logo";
+import PointerElement from "../../components/PointerElement";
 
 interface WhealProps {
     items: Array<object>;
@@ -14,6 +15,7 @@ const Wheal = (props: WhealProps) => {
     return (
         <div className="flex bg-gray-900 h-screen items-center justify-center">
             <Logo />
+            <PointerElement />
             <ul className="relative rounded-full border-slate-800 w-[80vh] h-[80vh] bg-black aspect-square overflow-hidden">
                 {items.map((item: any, index: number) => (
                     <WedgeLabel key={index} {...item} rotation={360 / items.length * index} height={height} />
