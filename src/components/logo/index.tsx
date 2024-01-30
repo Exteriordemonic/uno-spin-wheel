8290b10099f6933ac3f838ab34bab450a365e0bc
Kamil Mirosz <kamil.mirosz@nopio.com>
2024-01-30 20:00:19 +0100

    Update pointer-events in logo and wedge-label components

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,7 +3,7 @@ import LogoPNG from '../../assets/images/logo.png';
 
 const Logo = () => {
     return (
-        <div className="flex items-center justify-center absolute top-0 bottom-0 right-0 left-0 m-auto z-10">
+        <div className="flex items-center justify-center absolute top-0 bottom-0 right-0 left-0 m-auto z-10 pointer-events-none">
             <img src={LogoPNG} alt="logo" className="w-[150px] h-[150px] rounded-full" />
         </div>    
     )
