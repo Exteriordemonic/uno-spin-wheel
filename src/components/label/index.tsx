e5ac8ff518ce3308488611f045519f57928cab40
Kamil Mirosz <kamil.mirosz@nopio.com>
2024-02-03 21:55:08 +0100

    Update label component styles

--- src/components/label/index.tsx
+++ src/components/label/index.tsx
@@ -6,7 +6,7 @@ const Label = (props: LabelPropsType) => {
 
     return (
         <button
-            className={`w-full text-left flex justify-between items-center p-3 text-base font-bold text-gray-900 rounded-lg ${item.active ? '' : 'opacity-50 hover:opacity-100'}  hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white`}
+            className={`w-full text-left flex justify-between items-center p-3 text-base font-bold text-gray-900 rounded-lg ${item.active ? '' : 'text-gray-900/50 opacity-50 hover:opacity-100'}  hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white`}
             onClick={() => { handleLabelClick(item.id) }}
         >
             <span className="flex-1 ms-3 whitespace-nowrap">{item.label}</span>
